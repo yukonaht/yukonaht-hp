3fdeda78e0280f19bd091fb0550141d958383637
talus <yamamoto.yuujirou@mito1-h.ibk.ed.jp>
2024-12-09 10:18:30 +0900

    video type mp4 -> webm

--- souketsu-game/main.js
+++ souketsu-game/main.js
@@ -1,7 +1,7 @@
 const buttonID = document.getElementById("b1");
 buttonID.addEventListener("click", () => {
     let videoStageNum = buttonID.value;
-    document.getElementById("v1").innerHTML = `<video src="videos/STAGE${videoStageNum}.mp4" autoplay muted id="overlap"></video>`;
+    document.getElementById("v1").innerHTML = `<video src="videos/STAGE${videoStageNum}.webm" autoplay muted id="overlap"></video>`;
     document.getElementById("overlap").addEventListener("ended", () => {
         document.getElementById("v1").innerHTML = '';
     });
