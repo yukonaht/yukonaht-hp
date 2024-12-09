9b175913e3c40bfc8124fc859d5e13fb9e25b125
talus <yamamoto.yuujirou@mito1-h.ibk.ed.jp>
2024-12-09 14:59:58 +0900

    js change

--- souketsu-game/main.js
+++ souketsu-game/main.js
@@ -1,8 +1,9 @@
 const buttonID = document.getElementById("b1");
-buttonID.addEventListener("click", () => {
-    let videoStageNum = buttonID.value;
-    document.getElementById("v1").innerHTML = `<video src="videos/STAGE${videoStageNum}.webm" autoplay muted id="overlap"></video>`;
+buttonID.addEventListener("click",{name: buttonID.value, handleEvent: videoDisplay});
+
+function videoDisplay(e) {
+    document.getElementById("v-stage").innerHTML = `<video src="videos/STAGE${this.name}.mp4" autoplay muted id="overlap"></video>`;
     document.getElementById("overlap").addEventListener("ended", () => {
-        document.getElementById("v1").innerHTML = '';
+        document.getElementById("v-stage").innerHTML = '';
     });
-});
+}
