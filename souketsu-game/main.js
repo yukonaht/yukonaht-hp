d300658b4b0bed25e4fa2cfe2f934bda807048fc
talus <yamamoto.yuujirou@mito1-h.ibk.ed.jp>
2024-12-10 09:32:08 +0900

    div id:startScreen added

--- souketsu-game/main.js
+++ souketsu-game/main.js
@@ -1,7 +1,10 @@
+document.getElementById("startScreen").innerHTML = `<h1>倉頡輸入法ゲーム</h1>
+            <button id="b1" value="1">スタート</button>`;
 const buttonID = document.getElementById("b1");
-buttonID.addEventListener("click",{name: buttonID.value, handleEvent: videoDisplay});
+buttonID.addEventListener("click", { name: buttonID.value, handleEvent: videoPlay });
 
-function videoDisplay(e) {
+function videoPlay(e) {
+    document.getElementById("startScreen").innerHTML = '';
     document.getElementById("v-stage").innerHTML = `<video src="videos/STAGE${this.name}.mp4" autoplay muted id="overlap"></video>`;
     document.getElementById("overlap").addEventListener("ended", () => {
         document.getElementById("v-stage").innerHTML = '';
