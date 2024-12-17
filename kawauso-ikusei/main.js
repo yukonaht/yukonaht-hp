ec0381ee90dad4dbbea11d1fd719d06490687d75
talus <yamamoto.yuujirou@mito1-h.ibk.ed.jp>
2024-12-17 19:13:06 +0900

    js

--- kawauso-ikusei/main.js
+++ kawauso-ikusei/main.js
@@ -1,5 +1,10 @@
 let timeInterval = setInterval(timeDisplay, 50);
 
+function formatDate(date) {
+    const pad = (num) => String(num).padStart(2, '0');
+    return `${date.getFullYear()}/${pad(date.getMonth() + 1)}/${pad(date.getDate())} ${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
+}
+
 function timeDisplay() {
-    document.getElementById("time").innerHTML = new Date();
+    document.getElementById("time").innerHTML = formatDate(new Date());
 }
